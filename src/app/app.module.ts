611d6571e562abc9f9c93f0398cc918b5f86843b
adriansonaplane <109830657+adriansonaplane@users.noreply.github.com>
2022-09-02 07:58:23 -0700

    Added Skeleton Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from "@angular/common/http";
+
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+
+import {AppRoutingModule} from "./app-routing.module";
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { StudentComponent } from './components/student/student.component';
+import { GradeComponent } from './components/grade/grade.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { StudentsComponent } from './components/students/students.component';
+import { GradesComponent } from './components/grades/grades.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    StudentComponent,
+    GradeComponent,
+    NavBarComponent,
+    StudentsComponent,
+    GradesComponent,
   ],
   imports: [
-    BrowserModule
+
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
