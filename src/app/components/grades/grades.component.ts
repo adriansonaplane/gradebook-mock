08cddd95270ddccd99b0bd8d8f9c260b49428d80
zzyyzzzzy <zuojun_zheng@berkeley.edu>
2022-09-09 09:57:05 -0700

    fininsh styling + add delete grades

--- src/app/components/grades/grades.component.ts
+++ src/app/components/grades/grades.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Grade } from 'src/app/entities/grade';
 import { GradeService } from 'src/app/services/grade/grade.service';
+import {UserInfo} from "../../entities/userInfo";
+import {AuthService} from "../../services/auth/auth.service";
 
 @Component({
   selector: 'app-grades',
@@ -11,10 +13,12 @@ import { GradeService } from 'src/app/services/grade/grade.service';
 export class GradesComponent implements OnInit {
   sid: number = 0;
   gradeList:Grade[] = [];
+  curUser: UserInfo = this.authService.curUser;
   
   constructor(
     private route: ActivatedRoute,
-    private gradeService:GradeService
+    private gradeService:GradeService,
+    private authService: AuthService
     ) { }
 
   ngOnInit(): void {
@@ -22,6 +26,17 @@ export class GradesComponent implements OnInit {
 
     this.gradeService.getGradesByStudentId(this.sid).subscribe(grades => this.gradeList = grades);
   }
-
+  deleteGrade(id:number){
+    this.gradeService.deleteGrade(id).subscribe({
+      next: status => {
+          console.log(status);
+          alert("Delete successful")
+          location.reload();
+      },
+      error: error => {
+          console.error('There was an error!', error);
+      }
+  });;
+  }
 
 }
